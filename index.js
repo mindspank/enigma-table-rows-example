e75289b41b6a6caf1d862233724763ecf39f2d79
Alexander Karlsson <alexander.karlsson@qlik.com>
2017-02-03 17:32:09 -0500

    finishing up

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ request(config.request).then(result => {
     }))
 })
 .then(apps => {
-    console.log(apps)
     // Filter apps to only return apps with a specific table name
     // This looks a bit funky...
 
@@ -47,8 +46,34 @@ request(config.request).then(result => {
         })
     }, Promise.resolve([])) // <-- First iteration return a promise that resolves a empty array.
 })
-.then(appsContainingTable => console.log(appsContainingTable.length))
+.then(appsContainingTable => {
+    // We now have a list of apps containing the table we are looking for.
+    // Lets fetch some data for each app.
+    return Promise.all(appsContainingTable.map(app => {
+        return app.createSessionObject({
+            qInfo: {
+                qType: 'myobject'
+            },
+            qHyperCubeDef: {
+                qDimensions: [{ 
+                    qDef: { 
+                        qFieldDefs: ['Dim1'] 
+                    } 
+                }],
+                qInitialDataFetch: [{ qTop: 0, qLeft: 0, qWidth: 1, qHeight: 2000 }]
+            }
+        }).then(object => {
+            return object.getLayout()
+        })
+    }))
+})
+.then(pagesOfData => {
+    // do something
+    console.log(pagesOfData);
+    process.exit(1)
+})
 .catch(err => {
+    // Handle error
     console.log(err);
     process.exit(1)
 })
